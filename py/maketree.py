1ea84be9ac78dd97923fb87fd67e474a20cbf844
Martin Virtel <martin.virtel@gmail.com>
2013-05-10 07:47:42 +0200

    Initial Data

--- py/maketree.py
+++ py/maketree.py
@@ -1,10 +1,18 @@
+# coding: utf-8
 import csvstore
 import list2tree
+import os
+import simplejson
 
-entitats=csvstore.csvstore("unitatssac-scraped.csv")
+here=os.path.split(__file__)[0]
+
+datadir=os.path.join(here,"../data")
+
+
+entitats=csvstore.csvstore(os.path.join(datadir,"unitatssac-scraped.csv"))
 
 rootnode=list2tree.list2tree(filter(lambda a: a["nom"], entitats.data))
 
 pres=filter(lambda a: a["name"].find("Presidència")>-1,rootnode["children"])[0]
 
-list2tree.writetree(pres,"presidencia.json");
+simplejson.dump(pres,open(os.path.join(datadir,"presidencia.json"),"w"));
