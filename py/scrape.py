71d1e83c3eba7b5780e6628ba8b0b238a98acdc2
Martin Virtel <martin.virtel@gmail.com>
2013-05-10 08:11:55 +0200

    ReScraped

--- py/scrape.py
+++ py/scrape.py
@@ -1,9 +1,14 @@
 import csvstore
 import generalitat
 import simplejson
+import os
 
-generalitat.cache=simplejson.load(open("generalitat.cache.json"))
-entitats=csvstore.csvstore("unitatssac-scraped.csv")
+here=os.path.split(__file__)[0]
+
+datadir=os.path.join(here,"../data")
+
+generalitat.cache=simplejson.load(open(os.path.join(datadir,"generalitat.cache.json")))
+entitats=csvstore.csvstore(os.path.join(datadir,"unitatssac-scraped.csv"))
 try  :
 	for e in entitats.data : 
 		if not e["iddep-scraped"] : 
@@ -19,6 +24,6 @@ try  :
 			else :
 				print "%s -> %s" % (e["id"],e["iddep-scraped"])
 finally :
-	entitats.store("unitatssac-2013050-break-scraped-xml.csv")
-	simplejson.dump(generalitat.cache,open("generalitat.cache-out.json","w"))
+	entitats.store(os.path.join(datadir,"unitatssac-scraped-result.csv"))
+	simplejson.dump(generalitat.cache,open(os.path.join(datadir,"generalitat.cache-result.json"),"w"))
 	print "stored"
