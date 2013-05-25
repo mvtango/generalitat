490a6d597be0dc155d224b342b1b5832f79131ff
Martin Virtel <martin.virtel@gmail.com>
2013-05-25 19:55:19 +0200

    Make several trees, configurable
    Signed-off-by: Martin Virtel <martin.virtel@gmail.com>

--- py/maketree.py
+++ py/maketree.py
@@ -4,18 +4,36 @@ import list2tree
 import os
 import simplejson
 import sys
+import argparse
+
+
+parser = argparse.ArgumentParser(description="json tree generator", conflict_handler='resolve')
+parser.add_argument('infile',nargs="*",default="unitatssac-scraped-20130525.csv")
+args = vars(parser.parse_args())
+
+
 
 here=os.path.split(__file__)[0]
-
 datadir=os.path.join(here,"../data")
 
 
-entitats=csvstore.csvstore(os.path.join(datadir,"unitatssac-scraped-20130525.csv"))
+jobs= [ { 'find'  : 'Presidència',
+          'files' : [ os.path.join(datadir,"presidencia.json"),
+					  os.path.join(here,"../opengov.cat/generalitat/0.3/tree.json"),
+					  os.path.join(here,"../opengov.cat/generalitat/0.3/presidencia.json") ]
+		},
+		{ 'find'  : 'Agricultura',
+          'files' : [ os.path.join(datadir,"agricultura.json"),
+					  os.path.join(here,"../opengov.cat/generalitat/0.3/agricultura.json") ]
+		}
+	  ]
 
-rootnode=list2tree.list2tree(filter(lambda a: a["nom"], entitats.data),children="_children")
+for fn in args["infile"] :
+	entitats=csvstore.csvstore(os.path.join(datadir,fn))
+	rootnode=list2tree.list2tree(filter(lambda a: a["nom"], entitats.data),children="_children")
+	for job in jobs:
+		part=filter(lambda a: a["name"].find(job["find"])>-1,rootnode["_children"])[0]
+		for n in job["files"] :
+			print "written %s to %s" % (job["find"],n)
+			simplejson.dump(part,open(n,"w"));
 
-pres=filter(lambda a: a["name"].find("Presidència")>-1,rootnode["_children"])[0]
-
-
-for n in (os.path.join(datadir,"presidencia.json"), os.path.join(here,"../opengov.cat/generalitat/0.3/tree.json")) :
-	simplejson.dump(pres,open(n,"w"));
